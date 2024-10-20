3b8fad3c1e926ed27b200148d1e4d32318bc1318
mvthik <mvaathik@gmail.com>
2024-10-20 21:51:47 +0500

    Added an comment map and more telescope features

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,12 @@
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
-vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
+vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>fp", ":Telescope git_files<CR>")
 vim.keymap.set("n", "<leader>th", ":Telescope colorscheme<CR>")
+-- Formatting Keymap
+
 vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, {})
+-- Oil-nvim Keymaps
+
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- Force don't use arrows
 vim.keymap.set({ "n", "v", "i" }, "<Up>", "<Nop>", {})
@@ -17,3 +22,9 @@ vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = 
 vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
 vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false })
 vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false })
+
+-- Comment Keymap Bind
+vim.keymap.set({"n", "v"}, "<leader>/", ":CommentToggle<CR>")
+
+-- Command mode
+vim.keymap.set("n", ";", ":")
