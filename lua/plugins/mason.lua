bd2130d62821937536e1d2ad9c59413eaf380352
Thik17 <mvaathik@gmail.com>
2024-11-02 23:30:10 +0500

    Astro Bootstrap

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,6 +11,9 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
+        "pyright",
+        "ts_ls",
+        "clangd",
         -- add more arguments for adding more language servers
       },
     },
